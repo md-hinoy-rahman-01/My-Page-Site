75e5fa401333f7c2a135eac1768a16ee447c7fa6
Md Hinoy Rahman~ <mostafizurrahmanhinoy@gmail.com~>
2025-05-14 21:00:05 +0100

    Initial commit

---
+++ src/Navbar.jsx
@@ -0,0 +1,20 @@
+import "./Navbar.css"
+
+function Navbar()
+{
+    return(
+
+    <div className="navbar">
+        <h3 className="nav-home">My-Page</h3>
+        <h3 className="nav-item">Academics</h3>
+        <h3 className="nav-item">Experience</h3>
+        <h3 className="nav-item">Projects</h3>
+        <h3 className="nav-item">Contact</h3>
+    
+    </div>
+
+    )
+    
+}
+
+export default Navbar
