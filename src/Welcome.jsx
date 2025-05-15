c036e16a162163ae3b7e4c51d6dd1292f0b27261
H19OY <mostafizurrahmanhinoy@gmail.com~>
2025-05-15 14:58:34 +0100

    Welcome page got modified

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,7 +4,8 @@ function Welcome() {
   return (
     <div className="welcome">
       <h1>Welcome to the My-Page WebApp</h1>
-      <p>This is a simple React application.</p>
+      <p>This is a simple React application for now.</p>
+      <p>But quickly it see great improvement and will become something really interesting.</p>
     </div>
   );
 }   
